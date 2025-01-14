61c2fff1e3cbad8dc8496ed437d7c65f71157431
nithyaprakash-t <nithyaprakash.t@gmail.com>
2025-01-14 16:30:24 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # Description
-A template with reusable CSS resets, sample color schemes, typography, easings, scrollbars and icon files and more.
-
-Built on React for illustration purposes.
+A CRUD task management app
--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,11 +1,14 @@
 import { Themeswitch } from "../themeSwitch/Themeswitch";
+import { Link } from "react-router-dom";
 import './Header.scss';
 
 export function Header() {
     return (
         <header className="app-header__wrap app-grid">
             <div className="app-header__inner">
-                <p>App name</p>
+                <Link to={'/'} className='app-header__title' aria-label='Home - Nithya`s portfolio'>
+                    Taskly.
+                </Link>
                 <Themeswitch/>
             </div>
         </header>
